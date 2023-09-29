83946025c6f3b140c3b825f8c5a1fd895dc990d1
Андрей <kondrandr2004@yandex.ru>
2023-09-29 22:36:04 +0300

    Init commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token="12345678:AaBbCcDdEeFfGgHh")
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
