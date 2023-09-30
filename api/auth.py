505bb4c0f2f443c04d53a58b5f0128c1fda4dc4b
maj0rio <fadeevvanya@gmail.com>
2023-09-30 22:07:51 +0300

    c

--- api/auth.py
+++ api/auth.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from core.database import session
 from services.auth import auth_service
 from models.users import UserDB
+from models.tokens import TokenDB
 
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
@@ -10,4 +11,8 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 @auth_router.get('/')
 def sign_in(user_id: int, email: str, password: str):
+    user = session.query(UserDB).filter_by(user_id=user_id).first()
+    if user:
+        return session.query(TokenDB).filter_by(email=user.email).first().access_token
+
     return auth_service.sign_in(user_id, email, password)
