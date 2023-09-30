327f9ab95043d4ce0c3120f80f7f17734de15448
Андрей <kondrandr2004@yandex.ru>
2023-09-30 21:06:32 +0300

    Auth update

--- api/auth.py
+++ api/auth.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+from starlette import status
 
 from core.database import session
 from services.auth import auth_service
@@ -9,5 +10,15 @@ auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
 
 @auth_router.get('/')
-def sign_in(email: str, password: str):
+def sign_in(user_id: int, email: str, password: str):
+    user = session.query(UserDB).filter_by(email=email)
+    if user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Пользователь уже существует",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    user = UserDB(email=email, user_id=user_id)
+    session.add(user)
+    session.commit()
     return auth_service.sign_in(email, password)
