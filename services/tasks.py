61e5d8dccb1fc57e9c05f9519f413eb660d0c082
lisaanthro <liza_vater@yahoo.com>
2023-09-30 11:04:54 +0300

    Get task by id

--- services/tasks.py
+++ services/tasks.py
@@ -1,2 +1,21 @@
-def get_tasks(task_id: int):
-    pass
+import requests
+from sqlalchemy.orm import Session
+
+my_access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJvZGlvbnpha3JhdWxza2lqQGdtYWlsLmNvbSIsInVzZXJJZCI6IjY1MTJmOWVmODc5OTgyYzIwZjBhNGExZiIsImlhdCI6MTY5NjA1NTgwNCwiZXhwIjoxNjk2MzU1ODA0fQ.im7Mi9N-e79mF3kedbKFJeYcuSl7Sk9kj6yfo3bJ4s8"
+
+
+class TaskService:
+    API_URL = 'https://api.teamflame.ru/task'
+
+    def get_task_by_id(self, task_id: str):
+        access_token = my_access_token
+        spaces = requests.get(
+            url=self.API_URL + f'/{task_id}',
+            headers={
+                'accept': 'application/json',
+                'Authorization': f'Bearer {access_token}',
+            }
+        )
+        return spaces
+
+
